a2c534a14983119519b0ccb365afea80b1afa8a8
eraime906 <eraime906@fareham.ac.uk>
2023-03-07 10:54:24 +0000

    Username validity endpoint

--- src/main/java/org/healthadvicegroup/account/AccountManager.java
+++ src/main/java/org/healthadvicegroup/account/AccountManager.java
@@ -11,6 +11,9 @@ public class AccountManager {
 
     // A ConcurrentHashMap will allow us to save accounts asynchronously
     private static final ConcurrentHashMap<UUID, UserAccount> accountCache = new ConcurrentHashMap<>();
+
+    // Fast cache used to quickly determine whether a username exists or not
+    private static HashSet<String> usernameCache = new HashSet<>();
     private static final MongoCollectionWrapper accountCollection = MongoCollectionManager.getCollectionWrapper("accounts");
 
     public static void init() {
@@ -18,7 +21,9 @@ public class AccountManager {
         long start = System.currentTimeMillis();
         for (Document document : accountCollection.getAllDocuments()) {
             try {
-                accountCache.put(UUID.fromString(document.getString("_id")), new UserAccount(document));
+                UserAccount account = new UserAccount(document);
+                accountCache.put(UUID.fromString(document.getString("_id")), account);
+                usernameCache.add(account.getUsername().toLowerCase(Locale.ROOT));
             } catch (Exception ex) { // catch all possible exceptions when deserializing
                 System.out.printf("Failed to deserialize account with id %s\n", document.get("_id"));
             }
@@ -65,6 +70,17 @@ public class AccountManager {
      */
     public static UserAccount getAccount(UUID id) {
         return accountCache.get(id);
+    }
+
+    /**
+     * Returns whether an {@link UserAccount} exists with the provided {@link String} name
+     *
+     * @param username the username to check validity of
+     *
+     * @return whether an account exists with that username or not
+     */
+    public static boolean doesUsernameExist(String username) {
+        return usernameCache.contains(username.toLowerCase(Locale.ROOT));
     }
 
 }
--- src/main/java/org/healthadvicegroup/endpoint/impl/UsernameHeadEndpoint.java
+++ src/main/java/org/healthadvicegroup/endpoint/impl/UsernameHeadEndpoint.java
@@ -1,5 +1,6 @@
 package org.healthadvicegroup.endpoint.impl;
 
+import org.healthadvicegroup.account.AccountManager;
 import org.healthadvicegroup.endpoint.Endpoint;
 import spark.Request;
 import spark.Response;
@@ -8,8 +9,9 @@ public class UsernameHeadEndpoint extends Endpoint {
 
     @Override
     public Response handle(Request request, Response response) {
-        response.header("test", "this works!");
-        response.status(204);
+        String username = request.params(":username");
+        response.status(AccountManager.doesUsernameExist(username) ? 204 : 404);
+        response.header("sent-username", username);
         return response;
     }
 }
