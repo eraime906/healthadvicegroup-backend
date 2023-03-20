f16676dda11de6b4a93e9d6252ede779f521ed70
eraime906 <eraime906@fareham.ac.uk>
2023-03-20 10:56:56 +0000

    Rebuild Spark routes

--- src/main/java/org/healthadvicegroup/Main.java
+++ src/main/java/org/healthadvicegroup/Main.java
@@ -10,11 +10,13 @@ import org.healthadvicegroup.database.MongoCollectionManager;
 import org.healthadvicegroup.endpoint.EndpointManager;
 import org.healthadvicegroup.endpoint.impl.*;
 import org.healthadvicegroup.forecasting.ForecastManager;
-import spark.Spark;
+
+import static spark.Spark.*;
 
 public class Main {
 
-    @Getter private static Gson GSON;
+    @Getter
+    private static Gson GSON;
 
     @SuppressWarnings("DanglingJavadoc") // Used to document endpoints
     public static void main(String[] args) {
@@ -31,53 +33,20 @@ public class Main {
 
 
         // Construct API routes
-        /**
-         * Used to check whether an endpoint exists or not
-         *
-         * @see UsernameValidityEndpoint
-         */
-        Spark.head("/account/:username", (request, response) ->
-                EndpointManager.executeEndpoint(UsernameValidityEndpoint.class, request, response));
+        path("/", () -> {
+            before("/*", (request, response) -> {
+                System.out.printf("API call to %s from %s\n", request.url(), request.host());
+            });
+            path("/account", () -> {
+                head("/:username", (request, response) -> EndpointManager.executeEndpoint(UsernameValidityEndpoint.class, request, response));
+                post("/validate", (request, response) -> EndpointManager.executeEndpoint(CredentialsValidationEndpoint.class, request, response).body());
+                post("/create", (request, response) -> EndpointManager.executeEndpoint(AccountCreationEndpoint.class, request, response).body());
+            });
+            get("/articles", (request, response) -> EndpointManager.executeEndpoint(GetArticlesEndpoint.class, request, response).body());
+            get("/locations", (request, response) -> EndpointManager.executeEndpoint(GetLocationsEndpoint.class, request, response).body());
+            get("/location/:location", (request, response) -> EndpointManager.executeEndpoint(GetLocationDataEndpoint.class, request, response).body());
 
-        /**
-         * Used to validate whether the provided account credentials are valid or not
-         *
-         * @see CredentialsValidationEndpoint
-         */
-        Spark.post("account/validate", (request, response) ->
-                EndpointManager.executeEndpoint(CredentialsValidationEndpoint.class, request, response).body());
-
-        /**
-         * Used to create a new account
-         *
-         * @see AccountCreationEndpoint
-         */
-        Spark.post("account/create", (request, response) ->
-                EndpointManager.executeEndpoint(AccountCreationEndpoint.class, request, response).body());
-
-        /**
-         * Used to fetch all articles from {@link ArticleManager}
-         *
-         * @see GetArticlesEndpoint
-         */
-        Spark.get("/articles", (request, response) ->
-                EndpointManager.executeEndpoint(GetArticlesEndpoint.class, request, response).body());
-
-        /**
-         * Used to fetch a list of supported locations
-         *
-         * @see GetLocationsEndpoint
-         */
-        Spark.get("/locations", (request, response) ->
-                EndpointManager.executeEndpoint(GetLocationsEndpoint.class, request, response).body());
-
-        /**
-         * Used to fetch data about a location
-         *
-         * @see GetLocationDataEndpoint
-         */
-        Spark.get("/location/:location", (request, response) ->
-                EndpointManager.executeEndpoint(GetLocationDataEndpoint.class, request, response).body());
+        });
 
         System.out.println("Backend Service Started");
     }
