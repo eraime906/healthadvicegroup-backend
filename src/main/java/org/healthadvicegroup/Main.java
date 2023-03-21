4f69951fed960628873d88681be52454cf6041eb
eraime906 <eraime906@fareham.ac.uk>
2023-03-21 09:27:26 +0000

    Enable CORS

--- src/main/java/org/healthadvicegroup/Main.java
+++ src/main/java/org/healthadvicegroup/Main.java
@@ -31,6 +31,23 @@ public class Main {
         ArticleManager.init();
         ForecastManager.init();
 
+        // Enable CORS
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.type("application/json");
+        });
 
         // Construct API routes
         path("/", () -> {
