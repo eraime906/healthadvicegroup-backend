7067ad1a7dbf708a83d2b8e06e803f060e5e3399
eraime906 <eraime906@fareham.ac.uk>
2023-03-21 11:56:25 +0000

    Impl separate username and email checks for account validation

--- src/main/java/org/healthadvicegroup/endpoint/impl/CredentialsValidationEndpoint.java
+++ src/main/java/org/healthadvicegroup/endpoint/impl/CredentialsValidationEndpoint.java
@@ -12,15 +12,19 @@ public class CredentialsValidationEndpoint extends Endpoint {
     @Override
     public Response handle(Request request, Response response) {
         JsonObject json = super.bodyToJsonObject(request.body());
-        if (!json.has("usernameOrEmail") || !json.has("password")) {
+        if (!json.has("username") || !json.has("email") || !json.has("password")) {
             response.body("Missing username/email or password field");
             response.status(400);
             return response;
         }
-        String usernameOrEmail = json.get("usernameOrEmail").getAsString();
+        String username = json.get("username").getAsString();
+        String email = json.get("email").getAsString();
         String password = json.get("password").getAsString();
-        UserAccount account = AccountManager.getByUsernameOrEmail(usernameOrEmail);
-        if (account == null || !account.verifyPassword(password)) {
+        UserAccount account = AccountManager.getByUsernameOrEmail(username);
+        if (account == null // No account found
+                || !account.getEmail().equalsIgnoreCase(email) // Mismatched email
+                || !account.getUsername().equalsIgnoreCase(username)  // Mismatched username
+                || !account.verifyPassword(password)) { // Invalid password
             // To uphold security we don't want to be specific with what was incorrect
             response.body("Invalid username, email or password");
             response.status(401);
